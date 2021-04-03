34fe6c51eb344c2a2c89da10532e041936c0ebca
Lokesh Kumawat <lokesh.kumawat143@gmail.com>
2021-04-03 09:16:26 +0530

    Added mvc to application

--- LKGGroup.Bookstore/LKGGroup.Bookstore/Startup.cs
+++ LKGGroup.Bookstore/LKGGroup.Bookstore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +23,8 @@ namespace LKGGroup.Bookstore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +47,7 @@ namespace LKGGroup.Bookstore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
